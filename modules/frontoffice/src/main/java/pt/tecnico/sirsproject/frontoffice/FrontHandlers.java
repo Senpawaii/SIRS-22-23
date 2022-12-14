43d1e1b8f4560406877ccf4a5f3c17b4f5e48699
ist192564 <ist192564@tecnico.ulisboa.pt>
2022-12-14 15:56:31 +0000

    Changed server to respond with Json

--- modules/frontoffice/src/main/java/pt/tecnico/sirsproject/frontoffice/FrontHandlers.java
+++ modules/frontoffice/src/main/java/pt/tecnico/sirsproject/frontoffice/FrontHandlers.java
@@ -7,14 +7,27 @@ import java.io.OutputStream;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 
 public class FrontHandlers {
     public static class PingHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange x) throws IOException {
-            String response = "Hello there from the Front-office!";
-            x.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+
+            JSONArray content = new JSONArray();
+            content.put("Hello there from the Front-office!");
+            content.put("The server is up and running :)");
+
+            String response = content.toString(1);  // the argument "1" formats each entry into a seperate line
+
+            x.getResponseHeaders()
+                // .add("Access-Control-Allow-Origin", "*")
+                .set("Content-type", "application/json");
             x.sendResponseHeaders(200, response.getBytes().length);
+
             OutputStream out = x.getResponseBody();
             out.write(response.getBytes());
             out.close();
